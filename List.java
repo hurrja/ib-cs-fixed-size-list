9f2902c1232373565b10291c773409a188240017
Jarmo Hurri <jarmo.hurri@iki.fi>
2020-11-15 08:27:46 +0200

    * basic methods ready

--- List.java
+++ List.java
@@ -1,11 +1,66 @@
-public class List <T>
+public class List <T extends Comparable<T>>
 {
   public List (T[] array)
   {
     elements = array;
     maxSize = array.length;
+    size = 0;
   }
   
-  int maxSize;
+  public boolean isFull ()
+  {
+    return size >= maxSize;
+  }
+  
+  public boolean isEmpty ()
+  {
+    // code to be written
+    return false;
+  }
+
+  public void addFirst (T element)
+  {
+    for (int i = size - 1; i >= 0; i--)
+      elements [i + 1] = elements [i];
+    elements [0] = element;
+    size++;
+  }
+  
+  public void addLast (T element)
+  {
+    // code to be written
+  }
+
+  public void add (int index)
+  {
+    // code to be written
+  }
+
+  public T removeLast ()
+  {
+    size--;
+    return elements [size];
+  }
+  
+
+  // code to be written
+  // public T removeFirst ()
+  // {
+  // }
+
+  public String toString ()
+  {
+    StringBuffer strBuf = new StringBuffer ();
+    for (int i = 0; i < size; i++)
+    {
+      strBuf.append (elements [i]);
+      if (i < size - 1)
+        strBuf.append (", ");
+    }
+    return strBuf.toString ();
+  }
+  
+  private int maxSize;
+  private int size;
   private T[] elements;
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,12 @@ public class Main
 {
   public static void main(String[] args)
   {
-    List list = new List (new Integer[10]);
+    List<Integer> list = new List<> (new Integer[10]);
+    for (int i = 2; i < 20; i += 2)
+      list.addFirst (i);
+
+    list.removeLast ();
+    
+    System.out.println (list);
   }
 }
